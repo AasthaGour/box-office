f2699cb72f52a4b14a4b2087cfa0c152e93cca4f
Aastha Gour <aastha211093@gmail.com>
2023-04-25 18:33:35 +0530

    installed react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Starred from './pages/Starred';
+
 function App() {
-  return <div>hello</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/starred" element={<Starred />} />
+
+        <Route path="*" element={<div>Not found</div>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
